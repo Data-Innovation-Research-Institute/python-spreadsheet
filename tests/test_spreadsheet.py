7186a4c4789811aac8e191f4ba49ea6d8c4f0ba0
Jeffrey Morgan <DrMorganJeffrey@gmail.com>
2018-11-09 10:09:38 +0000

    Add test.

--- tests/test_spreadsheet.py
+++ tests/test_spreadsheet.py
@@ -8,6 +8,8 @@ class SpreadsheetTests(unittest.TestCase):
     def test_cells(self):
         spreadsheet = Spreadsheet()
         self.assertEqual(spreadsheet.current_cell(), 'A1')
+        spreadsheet.set_value('1')
+        self.assertEqual(spreadsheet.current_cell(), 'B1')
 
 
 if __name__ == '__main__':
